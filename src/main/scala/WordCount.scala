93dd50c9a6876f1d96cd4dd274f274ea28d318de
Vyun Sergey <vyun.sergey@icloud.com>
2022-04-18 23:09:23 +0300

    fix main add .gitignore

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,12 +1,18 @@
 import org.apache.spark._
 
-object WordCount {
-  val conf: SparkConf = new SparkConf(true).setMaster("local").setAppName("word-count")
-  val spark: SparkContext = SparkContext.getOrCreate(config = conf)
+import scala.util.Try
 
-  spark.textFile("hdfs://namenode1/usr/user/data/input")
-    .flatMap(line => line.split(" "))
-    .map(word => (word.trim, 1))
-    .reduceByKey(_ + _)
-    .saveAsTextFile("hdfs://namenode1/usr/user/data/output")
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val inputPath: Option[String] = Try(args(0)).toOption
+    val outputPath: Option[String] = Try(args(1)).toOption
+    val conf: SparkConf = new SparkConf(true).setMaster("local").setAppName("word-count")
+    val spark: SparkContext = SparkContext.getOrCreate(config = conf)
+
+    spark.textFile(inputPath.getOrElse("hdfs://namenode1/usr/user/data/input"))
+      .flatMap(line => line.split(" "))
+      .map(word => (word.trim, 1))
+      .reduceByKey(_ + _)
+      .saveAsTextFile(outputPath.getOrElse("hdfs://namenode1/usr/user/data/output"))
+  }
 }
